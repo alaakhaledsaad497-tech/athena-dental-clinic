11afab2bc83f89ff3f3dfac19398058588615d4d
alaakhaledsaad497-tech <alaakhaledsaad497@gmail.com>
2026-09-16 19:30:20 +0300

    .

--- script.js
+++ script.js
@@ -207,7 +207,7 @@ const translations = {
             "A professional team caring for your smile.",
 
         doctor1Name: "Dr. Ahmed Okl",
-        doctor2Name: "Dr. Basant Refaat",
+        doctor2Name: "Dr. passant Refaat",
         doctor3Name: "Dr. Nourhan Tarik",
         doctor4Name: "Dr. Mohamed Mansour",
         doctor5Name: "Dr. Noran Tarek",
